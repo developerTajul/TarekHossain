ed29cbb9da87657c1030aed123abe075ed86250d
Tarek Hossain <devlopertarek433@gmail.com>
2023-11-14 19:00:30 +0600

    today work details

--- class-7/js/script.js
+++ class-7/js/script.js
@@ -1,20 +1,48 @@
 
-    var swiper = new Swiper(".slider_area", {
-      spaceBetween: 30,
-      loop:true,
-      fade:true,
-     //  centeredSlides: true,
-      autoplay: {
-        delay: 1500,
-     //    disableOnInteraction: false,
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
+    // var swiper = new Swiper(".slider_area", {
+    //   spaceBetween: 30,
+    //   loop:true,
+    //   fade:true,
+    //  //  centeredSlides: true,
+    //   autoplay: {
+    //     delay: 1500,
+    //  //    disableOnInteraction: false,
+    //   },
+    //   pagination: {
+    //     el: ".swiper-pagination",
+    //     clickable: true,
+    //   },
+    //   navigation: {
+    //     nextEl: ".swiper-button-next",
+    //     prevEl: ".swiper-button-prev",
+    //   },
+    // });
+    // owl carsoule
+    $(document).ready(function(){
+        $('.owl-carousel').owlCarousel({
+          loop:true,
+          nav:true,
+          dots: true,
+          autoPlay:true,
+          lazyLoad:true,
+          autoplay:{
+            autoPlay:1000,
+            stopOnHover : false
+          },
+          navText: ['<i class="fa-solid fa-arrow-right-long" id="Next"></i>','<i class="fa-solid fa-arrow-left-long" id="Prev"></i>'],
+
+         
+          responsive:{
+              0:{
+                  items:1
+              },
+              600:{
+                  items:1
+              },
+              1000:{
+                  items:1
+              }
+          }
+      })
+    })
   
