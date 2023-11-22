a5f7372aa5eee2e83e0693547f19b7ffd38f84c2
Tarek Hossain <devlopertarek433@gmail.com>
2023-11-22 19:30:51 +0600

    today work

--- 11-20-23/js/main.js
+++ 11-20-23/js/main.js
@@ -1,38 +1,43 @@
 $(document).ready(function(){
      $('.slider-active').slick({
-          dots: false,
-          infinite: true,
-          arrows:false,
-          speed: 300,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          responsive: [
-            {
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: false,
-                dots: false
-              }
-            },
-            {
-              breakpoint: 600,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
-            }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-          ]
+      autoplay: true,
+      autoplaySpeed: 1000,
+      dots: false,
+      infinite: false,
+      speed: 1000,
+      arrows:false,
+      fade: true,
+      slide: 'div',
+      cssEase: 'linear'
         });
+        // counter js
+        $('.counter').counterUp({
+          delay: 10,
+          time: 1000
+      });
+      // testimonial slider
+      $('.testimonial-active').slick({
+        autoplay: true,
+        autoplaySpeed: 1000,
+        dots: true,
+        arrows:false,
+        infinite: false,
+        speed: 1000,
+        // fade: true,
+       
+        // cssEase: 'linear'
+      });
+      // brands active
+      $('.brands-item').slick({
+        autoplay: true,
+        autoplaySpeed: 1000,
+        dots: false,
+        arrows:false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true,
+        variableWidth: true
+      });
 })
