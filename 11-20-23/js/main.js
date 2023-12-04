fbc7820b5c081a3d17417d7a30c2ba258094df81
Tarek Hossain <devlopertarek433@gmail.com>
2023-12-04 11:36:06 +0600

    project-2

--- 11-20-23/js/main.js
+++ 11-20-23/js/main.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
+  // sticky
+  // $(window).on('scroll',function(){
+  //   var scroll = $(window).scrollTop();
+  //   if(scroll < 1){
+  //     $('sticky').removeClass('scroll-header');
+  //   }else{
+  //     $('sticky').addClass('scroll-header');
+  //   }
+  // })
+ 
+    // mobile menu
+    $('#mobile_menu').meanmenu({
+        meanScreenWidth: "991",
+        meanMenuContainer: ".mobile-menu",
+    });
      $('.slider-active').slick({
       autoplay: true,
       autoplaySpeed: 1000,
@@ -23,21 +38,89 @@ $(document).ready(function(){
         arrows:false,
         infinite: false,
         speed: 1000,
+        responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              },
+            },
+            {
+              breakpoint: 1008,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              },
+            },
+            {
+              breakpoint: 800,
+              settings: {
+                dots:false,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                },
+              },
+          ],
         // fade: true,
        
         // cssEase: 'linear'
       });
       // brands active
-      $('.brands-item').slick({
-        autoplay: true,
-        autoplaySpeed: 1000,
-        dots: false,
-        arrows:false,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        centerMode: true,
-        variableWidth: true
-      });
+      $('.brands-item').owlCarousel({
+        loop:true,
+        autoplaySpeed:1000,
+        items:5,
+        nav:false,
+        autoplay:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+    // scroll up js
+    $.scrollUp({
+        scrollName: 'scrollUp',      // Element ID
+        scrollDistance: 300,         // Distance from top/bottom before showing element (px)
+        scrollFrom: 'top',           // 'top' or 'bottom'
+        scrollSpeed: 300,            // Speed back to top (ms)
+        easingType: 'linear',        // Scroll to top easing (see http://easings.net/)
+        animation: 'slide',           // Fade, slide, none
+        animationSpeed: 200,         // Animation speed (ms)
+        scrollTrigger: false,        // Set a custom triggering element. Can be an HTML string or jQuery object
+        scrollTarget: false,         // Set a custom target element for scrolling to. Can be element or number
+        scrollText: '<i class="fa-solid fa-arrow-up"></i>', // Text for element, can contain HTML
+        scrollTitle: false,          // Set a custom <a> title if required.
+        scrollImg: false,            // Set true to use image
+        activeOverlay: false,        // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+        zIndex: 2147483647           // Z-Index for the overlay
+    });
+    // wow js
+    new WOW().init();
 })
+ // JavaScript to handle sticky behavior
+ window.onscroll = function() {
+  // Get the header
+  var header = document.querySelector('header');
+  
+  // Add the "sticky" class to the header when scrolling down, remove it when scrolling up
+  if (window.pageYOffset > 50) {
+    header.classList.add('sticky');
+  } else {
+    header.classList.remove('sticky');
+  }
+};
---
+++ 11-21-23/js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+     // counter js
+	$('.counter').counterUp({
+          delay: 10,
+          time: 1000
+     });
+     // magnific popup
+     
+})
