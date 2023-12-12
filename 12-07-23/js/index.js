9f2776fed1f107110cc575387b4a38ea4d99e31b
Tarek Hossain <devlopertarek433@gmail.com>
2023-12-12 18:46:43 +0600

    card

---
+++ 12-07-23/js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+     // counter js
+	$('.counter').counterUp({
+          delay: 10,
+          time: 1000
+     });
+     // magnific popup
+     $('.popup-player').magnificPopup({
+          type: 'iframe'
+          // other options
+        });
+})
