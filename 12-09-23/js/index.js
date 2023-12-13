8d19ae8b9d90fd150515df016a66f2cb95666952
Tarek Hossain <devlopertarek433@gmail.com>
2023-12-13 18:59:08 +0600

    work details

--- 12-09-23/js/index.js
+++ 12-09-23/js/index.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
      // counter js
-	// $('.counter').counterUp({
-     //      delay: 10,
-     //      time: 1000
-     // });
+	$('.counter').counterUp({
+          delay: 10,
+          time: 1000
+     });
      // magnific popup
      $('.popup-player').magnificPopup({
           type: 'iframe'
